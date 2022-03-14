88de732fcb6288464b81c502961bed8c17caa60d
cartergomez <cjgomez7911@gmail.com>
2022-03-14 14:12:49 +0000

    hi

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -39,13 +39,45 @@ var level01 = function (window) {
         sawBladeHitZone.addChild(obstacleImage); // add the image to the hitzone so we can see it
         obstacleImage.x = -25; //tweaks the image 25 pixels to the left 
         obstacleImage.y = -25; //tweaks the image 25 pixels up
+        sawBladeHitZone.rotationalVelocity = 5;
         }
 
-        createSawBlade(400, 345);
         createSawBlade(600, 345);
-        createSawBlade(800, 345);
+        createSawBlade(1500, 345);
+        createSawBlade(2000, 345);
 
+        function createEnemy(x,y){
+            var enemy = game.createGameItem('enemy',25); //creating the game item and storing it in the variable enemy
+            var redSquare = draw.rect(50,50,'red'); //creates rectangle and stores as redSquare
+            redSquare.x = -25;
+            redSquare.y = -25;
+            enemy.addChild(redSquare); //adds the redsquare to the enemy game
 
+            enemy.x = x;
+            enemy.y = y;
+
+            game.addGameItem(enemy);//adds enemy to the game
+
+            enemy.velocityX = -1; //this causes the enemy to move one pixel to the left on the x position
+            
+            enemy.rotationalVelocity = 25;
+
+            enemy.onPlayerCollision = function(){
+                console.log("The enemy has hit Halle");
+                game.changeIntegrity(-10);
+            };
+
+            enemy.onProjectileCollision = function(){
+                console.log("The projectile has hit Halle");
+                game.changeIntegrity(5);
+                game.increaseScore(10);
+                enemy.shrink();
+            };
+        }
+
+        createEnemy(400, groundY - 50);
+        createEnemy(800, groundY - 50);
+        createEnemy(1000, groundY - 50);
         // DO NOT EDIT CODE BELOW HERE
     }
 };
